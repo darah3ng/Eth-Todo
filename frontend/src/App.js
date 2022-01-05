87e791cc67afd36b55ada5bd92fad627c21d2566
darah3ng <daragitgud@gmail.com>
2022-01-05 14:05:40 +1000

    feat: toggle task completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ function App() {
   const [todoListState, setTodoListState] = useState();
   const [taskCountState, setTaskCountState] = useState();
   const [taskState, setTaskState] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     loadBlockchainData();
@@ -35,7 +38,42 @@ function App() {
       const task = await todoList.methods.tasks(i).call();
       setTaskState(prevState => [...prevState, task]);
     }
-  }
+  };
+
+  const createTask = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    todoListState.methods.createTask(taskInput)
+      .send({ from: yourAccount })
+      .once('receipt', receipt => {
+        setLoading(false);
+
+        console.log('create task receipt', receipt);
+      })
+      .on('error', error => {
+        setLoading(false);
+        setErrorMessage(error.message);
+
+        console.log('error: ', error);
+      });
+  };
+
+  const toggleCompleted = (taskId) => {
+    setLoading(true);
+
+    todoListState.methods.toggleCompleted(taskId)
+      .send({ from: yourAccount })
+      .once('receipt', receipt => {
+        console.log('toggle completed receipt', receipt);
+        setLoading(false);
+      })
+      .on('error', error => {
+        console.log('error: ', error);
+        setLoading(false);
+        setErrorMessage(error.message);
+      });
+  };
 
   return (
     <div className='App'>
@@ -43,11 +81,23 @@ function App() {
       <p>Your address: {yourAccount}</p>
       <p>Task count: {taskCountState}</p>
 
+      <form onSubmit={createTask}>
+        <label>Task: <input type={'text'} value={taskInput} onChange={e => setTaskInput(e.target.value)} /></label>
+        <input type={'submit'} hidden={true} />
+      </form>
+
+      <p>{loading ? <span>Loading ⌛</span> : errorMessage ? <span style={{ color: 'red' }}>{ errorMessage }</span> : null}</p>
+
       {(taskState || []).map((task, key) => {
         return (
           <div key={key}>
             <label>
-              <input type={'checkbox'} />
+              <input 
+                type={'checkbox'}
+                name={task.id}
+                defaultChecked={task.completed}
+                onClick={() => toggleCompleted(task.id)}
+              />
               <span>{task.content}</span>
             </label>
           </div>
